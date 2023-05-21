2490623b60d98ea03b08fe30d4323d67df671a46
AleksKen <aleks.ken2017@mail.ru>
2023-05-21 16:47:50 +0300

    open

--- src/model/model-waypoint.js
+++ src/model/model-waypoint.js
@@ -1,4 +1,4 @@
-import { getRandomPoint } from '../mock/point';
+import {getRandomPoint} from '../mock/point';
 
 const POINT_COUNT = 5;
 
--- src/view/edit-form.js
+++ src/view/edit-form.js
@@ -139,7 +139,7 @@ export default class EditForm {
   #element = null;
   #oneWaypoint = null;
 
-  constructor({oneWaypoint}) {
+  constructor(oneWaypoint) {
     this.#oneWaypoint = oneWaypoint;
   }
 
